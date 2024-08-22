e9bfd492ef21dac994e59c2644b8729060aa1fed
Gabriel Tavares <gabrielctavares.dev@gmail.com>
2024-08-22 00:48:52 -0300

    feat: implementação das classes Iphone e Nokia herdando de Smartphone e sobrescrita do método InstalarAplicativo
    - Criadas as classes Iphone e Nokia, ambas herdando da classe abstrata Smartphone.
    - Implementado o construtor para as duas classes, inicializando as propriedades herdadas.
    - Sobrescrita do método abstrato InstalarAplicativo para exibir mensagens personalizadas durante a instalação dos aplicativos.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+
+Nokia telefone = new Nokia("99291", "Nokia 2201", "123456789", 16);
+Iphone iphone = new Iphone("99231", "Iphone 16", "987654321", 256);
+
+telefone.Ligar();
+telefone.ReceberLigacao();
+telefone.InstalarAplicativo("Whatsapp");
+
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
